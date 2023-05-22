ecd094dd46c2ecd5e7f35ac193752cf21772cd89
vitor-galdino <httpvittor@gmail.com>
2023-05-22 20:10:51 -0300

    feat: add express-async-errors import and handleErrors as middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,10 @@
+import 'express-async-errors';
 import express, { Application } from 'express';
+import { handleErrors } from './middlewares/handleErrors.middleware';
 
 export const app: Application = express();
 app.use(express.json());
+
+
+
+app.use(handleErrors);
