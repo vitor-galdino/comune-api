a9e9af61990039d614389d323e355dd1a4493ec1
vitor-galdino <httpvittor@gmail.com>
2023-05-22 20:52:47 -0300

    feat: add Contact entity

---
+++ src/entities/contact.entity.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Customer } from './customer.entity';
+
+@Entity('contact')
+export class Contact {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 120 })
+  fullName: string;
+
+  @Column({ length: 45, unique: true })
+  email: string;
+
+  @Column({ length: 14 })
+  phone: string;
+
+  @CreateDateColumn({ type: 'date' })
+  createdAt: string;
+
+  @ManyToOne(() => Customer, customer => customer.contacts)
+  customer: Customer;
+}
