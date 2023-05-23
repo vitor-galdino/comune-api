13e5df2f620986fa336ce00d53c8b68054e23c65
vitor-galdino <httpvittor@gmail.com>
2023-05-23 16:20:56 -0300

    feat: config customers route for express

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import 'express-async-errors';
 import express, { Application } from 'express';
 import { handleErrors } from './middlewares/handleErrors.middleware';
+import { customersRoutes } from './routes/customers.routes';
 
 export const app: Application = express();
 app.use(express.json());
 
-
+app.use('/customers', customersRoutes);
 
 app.use(handleErrors);
