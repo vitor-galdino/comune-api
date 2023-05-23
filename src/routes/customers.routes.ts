9a2a9ceb544b60fca58234a8dc843fb63da6b18f
vitor-galdino <httpvittor@gmail.com>
2023-05-23 20:58:13 -0300

    feat: add get route to list a customer

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -4,8 +4,10 @@ import { Customer } from '../entities/customer.entity';
 import { validateBody } from '../middlewares/validateBody.middleware';
 import { validateEmail } from '../middlewares/validateEmail.middleware';
 import { customerSchema } from '../schemas/customer.schemas';
+import { ensureCustomerExist } from '../middlewares/ensureCustomerExist.middleware';
 
 export const customersRoutes: Router = Router();
 
 customersRoutes.post('', validateBody(customerSchema), validateEmail(Customer), createCustomerController);
 customersRoutes.get('', listCustomersController);
+customersRoutes.get('/:customerId', ensureCustomerExist, getCustomerController);
