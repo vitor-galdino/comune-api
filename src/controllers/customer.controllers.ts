bcf48bcab7754527d840545b4513d827f2a7826d
vitor-galdino <httpvittor@gmail.com>
2023-05-23 20:58:53 -0300

    feat: add getCustomerController

--- src/controllers/customer.controllers.ts
+++ src/controllers/customer.controllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { createCustomerService } from '../services/customer/createCustomer.service';
+import { getCustomerService } from '../services/customer/getCustomer.service';
 import { listCustomersService } from '../services/customer/listCustomers.service';
 
 export const createCustomerController = async (req: Request, res: Response): Promise<Response> => {
@@ -8,6 +9,12 @@ export const createCustomerController = async (req: Request, res: Response): Pro
 };
 
 export const listCustomersController = async (req: Request, res: Response): Promise<Response> => {
-  const customersData = await listCustomersService()
-  return res.json(customersData)
-}
+  const customersData = await listCustomersService();
+  return res.json(customersData);
+};
+
+export const getCustomerController = async (req: Request, res: Response): Promise<Response> => {
+  const id: number = parseInt(req.params.customerId);
+  const customersData = await getCustomerService(id);
+  return res.json(customersData);
+};
