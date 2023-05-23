6ab8d2c10fd53b95790529e1d3b12bea20c75cb9
vitor-galdino <httpvittor@gmail.com>
2023-05-23 18:52:27 -0300

    refactor: add new param to receive entity

--- src/middlewares/validateEmail.middleware.ts
+++ src/middlewares/validateEmail.middleware.ts
@@ -1,11 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
-import { Repository } from 'typeorm';
+import { EntityTarget, Repository } from 'typeorm';
 import { AppDataSource } from '../data-source';
-import { Customer } from '../entities/customer.entity';
 import { AppError } from '../errors/AppError';
 
-export const validateEmail = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-  const customerRepos: Repository<Customer> = AppDataSource.getRepository(Customer);
+export const validateEmail = (entity: any) => async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+  const customerRepos: Repository<any> = AppDataSource.getRepository(entity);
 
   const isExist = await customerRepos.findOne({
     where: { email: req.body.email }
--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createCustomerController, listCustomersController } from '../controllers/customer.controllers';
+import { createCustomerController, getCustomerController, listCustomersController } from '../controllers/customer.controllers';
+import { Customer } from '../entities/customer.entity';
 import { validateBody } from '../middlewares/validateBody.middleware';
 import { validateEmail } from '../middlewares/validateEmail.middleware';
 import { customerSchema } from '../schemas/customer.schemas';
 
 export const customersRoutes: Router = Router();
 
-customersRoutes.post('', validateBody(customerSchema), validateEmail, createCustomerController);
+customersRoutes.post('', validateBody(customerSchema), validateEmail(Customer), createCustomerController);
 customersRoutes.get('', listCustomersController);
