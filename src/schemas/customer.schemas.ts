0118b7ddfcc258cdf186b82e633bdefb47e7187c
vitor-galdino <httpvittor@gmail.com>
2023-05-23 15:36:13 -0300

    feat: add schemas with zod from Customer

---
+++ src/schemas/customer.schemas.ts
@@ -0,0 +1,15 @@
+import { string, z } from 'zod';
+
+export const customerSchema = z.object({
+  fullName: z.string().min(3).max(120),
+  email: z.string().email().max(45),
+  phone: z.string().max(14),
+});
+
+export const customerResponseSchema = z.object({
+  id: z.number(),
+  ...customerSchema.shape,
+  createdAt: string(),
+});
+
+export const allCustomersSchema = customerResponseSchema.array();
