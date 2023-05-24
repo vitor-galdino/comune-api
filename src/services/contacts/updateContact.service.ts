52dc6e0d16e0b5e496283f65547a7d5576efd78c
vitor-galdino <httpvittor@gmail.com>
2023-05-24 12:01:48 -0300

    feat: add updateContactService

---
+++ src/services/contacts/updateContact.service.ts
@@ -0,0 +1,20 @@
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../../data-source';
+import { Contact } from '../../entities/contact.entity';
+import { AppError } from '../../errors/AppError';
+import { tContactResponse, tContactUpdate } from '../../interfaces/contact.interfaces';
+import { contactResponseSchema } from '../../schemas/contact.schemas';
+
+export const updateContactService = async (customerId: number, payload: tContactUpdate): Promise<tContactResponse> => {
+
+  if (!Object.keys(payload).length) {
+    throw new AppError('Request body cannot be empty. Please provide a valid JSON object in the request body.');
+  }
+
+  const contactRepos: Repository<Contact> = AppDataSource.getRepository(Contact);
+
+  await contactRepos.update(customerId, payload);
+  const contactUpdated = await contactRepos.findOneBy({ id: customerId });
+
+  return contactResponseSchema.parse(contactUpdated);
+};
