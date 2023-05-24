108b4c16606e2c901718236cf9f6bcbcc01b8863
vitor-galdino <httpvittor@gmail.com>
2023-05-24 14:09:20 -0300

    feat: add and config reportsRaoutes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import express, { Application } from 'express';
 import { handleErrors } from './middlewares/handleErrors.middleware';
 import { contactsRoutes } from './routes/contacts.routes';
 import { customersRoutes } from './routes/customers.routes';
+import { reportsRoutes } from './routes/reports.routes';
 
 export const app: Application = express();
 app.use(express.json());
 
 app.use('/customers', customersRoutes);
 app.use('/customers/:customerId/contacts', contactsRoutes);
+app.use('/reports', reportsRoutes);
 
 app.use(handleErrors);
---
+++ src/routes/reports.routes.ts
@@ -0,0 +1,3 @@
+import { Router } from 'express';
+
+export const reportsRoutes: Router = Router();
