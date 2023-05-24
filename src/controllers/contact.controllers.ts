8caf0d3320a54d0de0c3b2357037b2a1a14a1586
vitor-galdino <httpvittor@gmail.com>
2023-05-24 11:32:53 -0300

    feat: add getContactByCustomerController

--- src/controllers/contact.controllers.ts
+++ src/controllers/contact.controllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { createContactService } from '../services/contacts/createContact.service';
 import { listContactsByCustomerService } from '../services/contacts/listContactsByCustomer.service';
+import { getContactByCustomerService } from '../services/contacts/getContactByCustomer.service';
 
 export const createContactController = async (req: Request, res: Response): Promise<Response> => {
   const customerId: number = parseInt(req.params.customerId);
@@ -11,5 +12,12 @@ export const createContactController = async (req: Request, res: Response): Prom
 export const listContactsByCustomerController = async (req: Request, res: Response): Promise<Response> => {
   const customerId: number = parseInt(req.params.customerId);
   const contactData = await listContactsByCustomerService(customerId);
+  return res.json(contactData);
+};
+
+export const getContactByCustomerController = async (req: Request, res: Response): Promise<Response> => {
+  const customerId: number = parseInt(req.params.customerId);
+  const contactId: number = parseInt(req.params.contactId);
+  const contactData = await getContactByCustomerService(customerId, contactId);
   return res.json(contactData);
 };
