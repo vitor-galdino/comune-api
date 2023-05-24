7a64d2130eed3e9587839f01855a7cce52b13039
vitor-galdino <httpvittor@gmail.com>
2023-05-23 21:33:33 -0300

    feat: add patch route

--- src/routes/customers.routes.ts
+++ src/routes/customers.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
-import { createCustomerController, getCustomerController, listCustomersController } from '../controllers/customer.controllers';
+import { createCustomerController, getCustomerController, listCustomersController, updateCustomerController } from '../controllers/customer.controllers';
 import { Customer } from '../entities/customer.entity';
+import { ensureCustomerExist } from '../middlewares/ensureCustomerExist.middleware';
 import { validateBody } from '../middlewares/validateBody.middleware';
 import { validateEmail } from '../middlewares/validateEmail.middleware';
-import { customerSchema } from '../schemas/customer.schemas';
-import { ensureCustomerExist } from '../middlewares/ensureCustomerExist.middleware';
+import { customerSchema, customerUpdateSchema } from '../schemas/customer.schemas';
 
 export const customersRoutes: Router = Router();
 
 customersRoutes.post('', validateBody(customerSchema), validateEmail(Customer), createCustomerController);
 customersRoutes.get('', listCustomersController);
 customersRoutes.get('/:customerId', ensureCustomerExist, getCustomerController);
+customersRoutes.patch('/:customerId', validateBody(customerUpdateSchema), ensureCustomerExist, validateEmail(Customer), updateCustomerController);
