db07d217f8b4057751275f55f7ff575c00c15acd
vitor-galdino <httpvittor@gmail.com>
2023-05-23 21:08:46 -0300

    refactor: add condition to check if has email in body

--- src/middlewares/validateEmail.middleware.ts
+++ src/middlewares/validateEmail.middleware.ts
@@ -1,17 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
-import { EntityTarget, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { AppDataSource } from '../data-source';
 import { AppError } from '../errors/AppError';
 
 export const validateEmail = (entity: any) => async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-  const repos: Repository<any> = AppDataSource.getRepository(entity);
+  if (req.body.email) {
+    const repos: Repository<any> = AppDataSource.getRepository(entity);
 
-  const isExist = await repos.findOne({
-    where: { email: req.body.email }
-  });
+    const isExist = await repos.findOne({
+      where: { email: req.body.email }
+    });
 
-  if (isExist) {
-    throw new AppError('Email already exists', 409);
+    if (isExist) {
+      throw new AppError('Email already exists', 409);
+    }
+    
+    return next();
   }
-  return next();
 }; 
