b992fd3e41676d4245d9f26582c008c972da4ac6
vitor-galdino <httpvittor@gmail.com>
2023-05-24 10:42:52 -0300

    feat: add contact post route

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -1,3 +1,11 @@
 import { Router } from 'express';
+import { createContactController } from '../controllers/contact.controllers';
+import { Contact } from '../entities/contact.entity';
+import { ensureCustomerExist } from '../middlewares/ensureCustomerExist.middleware';
+import { validateBody } from '../middlewares/validateBody.middleware';
+import { validateEmail } from '../middlewares/validateEmail.middleware';
+import { contactSchema } from '../schemas/contact.schemas';
 
-export const contactsRoutes: Router = Router();
+export const contactsRoutes: Router = Router({ mergeParams: true });
+
+contactsRoutes.post('', validateBody(contactSchema), validateEmail(Contact), ensureCustomerExist, createContactController);
