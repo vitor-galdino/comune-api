bab3b27d4ce5c0adcf4365498fe1b834157f76a6
vitor-galdino <httpvittor@gmail.com>
2023-05-24 15:53:36 -0300

    refactor: return a PDF report instead of a JSON object

--- src/controllers/report.controllers.ts
+++ src/controllers/report.controllers.ts
@@ -3,6 +3,11 @@ import { getReportService } from '../services/reports/getReport.service';
 
 export const getReportController = async (req: Request, res: Response): Promise<Response> => {
   const customerId: number = parseInt(req.params.customerId);
-  const reportData = await getReportService(customerId);
-  return res.json(reportData);
+  const [pdfBuffer, customerName] = await getReportService(customerId);
+  const fileName = `relatorio_cliente_${customerName.replace(' ', '_')}.pdf`;
+
+  res.setHeader('Content-Type', 'application/pdf');
+  res.setHeader('Content-Disposition', `attachment; filename=${fileName}`);
+
+  return res.send(pdfBuffer);
 };
