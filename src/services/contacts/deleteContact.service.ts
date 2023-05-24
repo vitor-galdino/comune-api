f5a1fe00c3fb4a83bedbba3942072f1ebeedaf14
vitor-galdino <httpvittor@gmail.com>
2023-05-24 13:50:27 -0300

    feat: add deleteContactService

---
+++ src/services/contacts/deleteContact.service.ts
@@ -0,0 +1,9 @@
+import { Repository } from 'typeorm';
+import { AppDataSource } from '../../data-source';
+import { Contact } from '../../entities/contact.entity';
+
+export const deleteContactService = async (contactId: number): Promise<void> => {
+  const contactRepos: Repository<Contact> = AppDataSource.getRepository(Contact);
+  const contactFound = await contactRepos.findOneBy({ id: contactId });
+  contactRepos.delete(contactFound!)
+};
