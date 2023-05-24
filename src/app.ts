bd8ac895767296c5012adfbcc0e6a9d5de29288d
vitor-galdino <httpvittor@gmail.com>
2023-05-23 23:29:25 -0300

    feat: create and config contactsRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'express-async-errors';
 import express, { Application } from 'express';
 import { handleErrors } from './middlewares/handleErrors.middleware';
+import { contactsRoutes } from './routes/contacts.routes';
 import { customersRoutes } from './routes/customers.routes';
 
 export const app: Application = express();
 app.use(express.json());
 
 app.use('/customers', customersRoutes);
+app.use('/contacts', contactsRoutes);
 
 app.use(handleErrors);
