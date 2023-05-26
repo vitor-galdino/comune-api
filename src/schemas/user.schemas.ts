c2a794d403164e8cfeb5f677917a0f5686583f62
vitor-galdino <httpvittor@gmail.com>
2023-05-26 20:29:44 -0300

    feat: add password key on zod schema

--- src/schemas/user.schemas.ts
+++ src/schemas/user.schemas.ts
@@ -4,13 +4,14 @@ export const userSchema = z.object({
   fullName: z.string().min(3).max(120),
   email: z.string().email().max(45),
   phone: z.string().max(14),
+  password: z.string().min(3).max(120),
 });
 
 export const userResponseSchema = z.object({
   id: z.number(),
   ...userSchema.shape,
   createdAt: z.string(),
-});
+}).omit({ password: true });
 
 export const allUsersSchema = userResponseSchema.array();
 
