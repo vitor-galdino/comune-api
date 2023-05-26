ca53593f5092748ea3d3d16ca1897ec919e66c88
vitor-galdino <httpvittor@gmail.com>
2023-05-26 20:21:17 -0300

    refactor: remove every email validation for contacts

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -1,16 +1,14 @@
 import { Router } from 'express';
 import { createContactController, deleteContactController, getContactByUserController, listContactsByUserController, updateContactController } from '../controllers/contact.controllers';
-import { Contact } from '../entities/contact.entity';
 import { ensureContactExist } from '../middlewares/ensureContactExist.middleware';
 import { ensureUserExist } from '../middlewares/ensureUserExist.middleware';
 import { validateBody } from '../middlewares/validateBody.middleware';
-import { validateEmail } from '../middlewares/validateEmail.middleware';
 import { contactSchema, contactUpdateSchema } from '../schemas/contact.schemas';
 
 export const contactsRoutes: Router = Router({ mergeParams: true });
 
-contactsRoutes.post('', validateBody(contactSchema), validateEmail(Contact), ensureUserExist, createContactController);
+contactsRoutes.post('', validateBody(contactSchema), ensureUserExist, createContactController);
 contactsRoutes.get('', ensureUserExist, listContactsByUserController);
 contactsRoutes.get('/:contactId', ensureUserExist, ensureContactExist, getContactByUserController);
-contactsRoutes.patch('/:contactId', validateBody(contactUpdateSchema), ensureUserExist, validateEmail(Contact), ensureContactExist, updateContactController);
+contactsRoutes.patch('/:contactId', validateBody(contactUpdateSchema), ensureUserExist, ensureContactExist, updateContactController);
 contactsRoutes.delete('/:contactId', ensureUserExist, ensureContactExist, deleteContactController);
