c2467b32ee0e1c086cf86f2d1d18ad0b33668bd5
vitor-galdino <httpvittor@gmail.com>
2023-05-26 20:38:21 -0300

    fix: pass contact id to delete method

--- src/services/contacts/deleteContact.service.ts
+++ src/services/contacts/deleteContact.service.ts
@@ -5,5 +5,5 @@ import { Contact } from '../../entities/contact.entity';
 export const deleteContactService = async (contactId: number): Promise<void> => {
   const contactRepos: Repository<Contact> = AppDataSource.getRepository(Contact);
   const contactFound = await contactRepos.findOneBy({ id: contactId });
-  contactRepos.delete(contactFound!)
+  contactRepos.delete(contactFound!.id);
 };
